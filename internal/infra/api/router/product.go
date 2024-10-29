9f408763dfe432f4b22f0fa2dbc5627073d05f4b
Yael <yaelsilvestre64@gmail.com>
2024-10-28 19:28:22 -0600

    ROUTER PRODUCT

---
+++ internal/infra/api/router/product.go
@@ -0,0 +1,28 @@
+package router
+
+import (
+	"github/thankeddeer/lastlayudas/internal/infra/api/handler"
+
+	"github.com/labstack/echo/v4"
+)
+
+type IProductRouter interface {
+	ProductResource(g *echo.Group)
+}
+
+type ProductRouter struct {
+	handler handler.IProductHandler
+}
+
+func NewProductRouter(handler handler.IProductHandler) IProductRouter {
+	return &ProductRouter{
+		handler: handler,
+	}
+}
+
+func (u *ProductRouter) ProductResource(g *echo.Group) {
+	groupPath := g.Group("/product")
+	groupPath.POST("/", u.handler.CreateProduct)
+	groupPath.GET("/", u.handler.GetAllProducts)
+	groupPath.PUT("/", u.handler.UpdateProduct)
+}
